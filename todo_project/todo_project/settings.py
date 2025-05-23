2f245f0b4bfb8c8a20806937438daf00e762b19a
vsvardhan967 <vsvardhan967@gmail.com>
2025-05-23 11:07:58 +0530

    Prepare for Render deployment

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3'),
     }
 }
 
